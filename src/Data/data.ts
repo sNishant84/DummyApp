a5c3ea5bdf7f02d23f9befd8d211202d070a4b5a
sNishant84 <snishant842@gmail.com>
2025-08-24 00:00:06 +0530

    Create data.ts

---
+++ src/Data/data.ts
@@ -0,0 +1,18 @@
+export const feedData=[
+    {
+        id:1,
+        name:'Theresa Web',
+        imgUrl:'/public/assets/profile1.png',
+        timeStamp:5,
+        postText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        emoji:'🥴',
+    },
+    {
+        id:2,
+        name:'Marvin Mckiney',
+        imgUrl:'/public/assets/profile2.png',
+        timeStamp:8,
+        postText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        emoji:'🤞',
+    }
+]
